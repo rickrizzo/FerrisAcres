babeedc8f6716c85d062293c5d8d60d5390a5aa1
Robert Russo <rrusso44@gmail.com>
2016-11-16 10:51:50 -0500

    Quick Home Page
    Something really quick home page prototype

--- app.js
+++ app.js
@@ -22,6 +22,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// static modules
+app.use('/modules', express.static(__dirname + '/node_modules/'));
+
 app.use('/', routes);
 app.use('/api', api);
 
