56b5901b499be062ef2f40cb3ce55b942d2a15b1
Robert Russo <rrusso44@gmail.com>
2016-11-23 10:59:49 -0500

    Posting to database!
    Now anyone anywhere can connect and use the database

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: DATABASE_URL=postgres://vmvwoaupaxwfue:wW6UT1crVGErnx6z8_p2ssfgtx@ec2-50-19-227-171.compute-1.amazonaws.com:5432/d2nic9l1t6vto2 npm start
+web: DATABASE_NAME=d2nic9l1t6vto2 DATABASE_URL=postgres://vmvwoaupaxwfue:wW6UT1crVGErnx6z8_p2ssfgtx@ec2-50-19-227-171.compute-1.amazonaws.com:5432/d2nic9l1t6vto2 npm start
--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -1,14 +1,6 @@
 const pg = require('pg');
 pg.defaults.ssl = true;
-pg.defaults.database = 'd2nic9l1t6vto2'
-console.log(pg.defaults);
-// const config = {
-//   database: 'ferris_acres',
-//   host: process.env.DATABASE_URL || 'localhost',
-//   port: '5432',
-//   ssl: true
-// }
-// const client = new pg.Client(config);
+pg.defaults.database = process.env.DATABASE_NAME || 'ferris_acres';
 
 module.exports = {
   createUser: function(req, res) {
@@ -18,25 +10,16 @@ module.exports = {
     req.body.phone = req.body.phone.replace(" ", "");
     req.body.phone = parseInt(req.body.phone);
     pg.connect(process.env.DATABASE_URL || 'localhost', (err, client) => {
-      console.log(process.env.DATABASE_URL);
       if (err) throw err;
-      console.log("CONNECTED TO POSTGRES!");
+      client.query('INSERT INTO users (name, email, phone) VALUES ($1, $2, $3);', [req.body.name, req.body.email, req.body.phone], (err, result) => {
+        if (err) {
+          if(err.message.includes('duplicate key')) {
+            console.log("Duplicate key");
+          }
+          console.log("Error: ", err);
+        }
+        client.end((err) => { if (err) console.log("Error: ", err); });
+      });
     });
   }
-
-
-  //   client.connect((err) => {
-  //     if (err) { console.log("Error!", err); return; }
-  //     client.query('INSERT INTO users (name, email, phone) VALUES ($1, $2, $3);', [req.body.name, req.body.email, req.body.phone], (err, result) => {
-  //       if (err) {
-  //         if(err.message.includes('duplicate key')) {
-  //           console.log("Duplicate key");
-  //         }
-  //         console.log("Error!", err);
-  //       }
-  //       client.end((err) => { if (err) console.log("Error!", err); });
-  //     });
-  //   });
-  //   res.json(req.body);
-  // }
 }
